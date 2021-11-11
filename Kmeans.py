85a0e157f01ad0738b0b62867385748525ea67f6
Abhiramv2 <93931031+Abhiramv2@users.noreply.github.com>
2021-11-12 04:56:12 +0530

    Update Kmeans.py

--- Kmeans.py
+++ Kmeans.py
@@ -1,25 +1,12 @@
-def random(ds, k, random_state=42):
-    """
-    Create random cluster centroids.
+def Kmeans(data, k, random_state):
     
-    Parameters
-    ----------
-    ds : numpy array
-        The dataset to be used for centroid initialization.
-    k : int
-        The desired number of clusters for which centroids are required.
-    Returns
-    -------
-    centroids : numpy array
-        Collection of k centroids as a numpy array.
-    """
 
     np.random.seed(random_state)
     centroids = []
-    m = np.shape(ds)[0]
+    m = np.shape(data)[0]
 
     for _ in range(k):
         r = np.random.randint(0, m-1)
-        centroids.append(ds[r])
+        centroids.append(data[r])
 
     return np.array(centroids)
